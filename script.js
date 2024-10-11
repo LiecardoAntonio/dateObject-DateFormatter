56fa98b22d3ec3bfb2f141314a2ad7b33621f753
LiecardoAntonio <liecardoantonio24@gmail.com>
2024-10-11 14:34:52 +0700

    progress1

---
+++ script.js
@@ -0,0 +1,22 @@
+const currentDateParagraph = document.getElementById('current-date');
+const dateOptionsSelectElement = document.getElementById('date-options');
+
+//create the new Date Object using new Date() constructor
+const date = new Date();
+console.log(date); //log the current date to console
+
+//date built-in methods
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+//creating a format for date
+const formattedDate = ``;
+
+
+
+
+
+
